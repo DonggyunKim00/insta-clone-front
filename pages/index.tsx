3cf039ff9c46783e72de292318b3f9e3924cdccc
donggyun <donggyunkim12@gmail.com>
2023-04-10 14:13:07 +0900

    초기설정

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function Home() {
+  return (
+    <div>
+      <Hi>hello</Hi>
+    </div>
+  );
+}
+
+const Hi = styled.div`
+  background-color: gray;
+`;
